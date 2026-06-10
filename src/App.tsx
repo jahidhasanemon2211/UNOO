ea1873231225baa9c06e44d04e9fe78c4b22fcba
Md Jahid Hasan Emon <jahidhasanemon2211@gmail.com>
2026-06-11 01:09:00 +0600

    Auto-commit on change

--- src/App.tsx
+++ src/App.tsx
@@ -375,9 +375,8 @@ export default function App() {
             </div>
           </div>
         </div>
-      </div>
-    );
-  }
+      );
+    }
 
   if (gameState.status === 'playing') {
     const isMyTurn = gameState.players[gameState.currentPlayerIndex]?.id === activeSocket.id;
